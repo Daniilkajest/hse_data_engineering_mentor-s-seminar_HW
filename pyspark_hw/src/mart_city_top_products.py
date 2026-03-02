91991366b13f4d0f48efb235935eab6170e544cd
Daniil <127546104+Daniilkajest@users.noreply.github.com>
2026-03-02 23:47:32 +0300

    Update mart_city_top_products.py

--- pyspark_hw/src/mart_city_top_products.py
+++ pyspark_hw/src/mart_city_top_products.py
@@ -1,1 +1,6 @@
-
+git init
+git add .
+git commit -m "Initial commit: setup project structure and baseline script"
+git branch -M main
+git remote add origin <https://github.com/Daniilkajest/hse_data_engineering_mentor-s-seminar_HW/tree/main/pyspark_hw>
+git push -u origin main
